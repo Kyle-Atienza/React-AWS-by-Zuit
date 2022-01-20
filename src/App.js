ce2654e48690341e245b0d31d8501559451bb1d4
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-01-20 15:58:49 +0800

    Apply external CSS

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <header className='app-header'>
+        <h1>My Amazing To-do list App</h1>
+        <p>The most simple and amazing todo-list React app.</p>
+      </header>
     </div>
   );
 }
