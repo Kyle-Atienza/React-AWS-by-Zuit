fc3d98d9d25fb8a18f897d8b94db7b5cd034296e
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-01-20 16:30:45 +0800

    Added inline CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
 function App() {
+  const pStyle = {
+    "marginBottom": "15px"
+  }
   return (
     <div className="app">
       <header className='app-header'>
         <h1>My Amazing To-do list App</h1>
-        <p>The most simple and amazing todo-list React app.</p>
+        <p style={pStyle}>The most simple and amazing todo-list React app.</p>
+        <ul>
+          <li className='tasks-item'>Follow Edukasyon.ph on Facebook</li>
+          <li className='tasks-item'>Follow AWS Skilab Pilipinas on Facebook</li>
+          <li className='tasks-item'>Follow Zuitt Coding Bootcamp on Facebook</li>
+        </ul>
       </header>
     </div>
   );
